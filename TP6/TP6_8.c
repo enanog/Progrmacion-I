2bb8b3b4ea989f7d42317046faec9edcc8004064
Dylan Ezequiel Frigerio <dyfrigerio@AWSNV-L-VDA03.itbadc.itba.edu.ar>
2025-04-24 10:30:54 -0300

    .

--- TP6/TP6_8.c
+++ TP6/TP6_8.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
+char* strSplit(char* str, char separador);
 
+int main(void)
+{
+	char str[] = "Hola,,paisano";
+	char* nstr;
+	
+	nstr = strSplit(str, ',');
+	
+	printf("%c\n", nstr[0]);
+	
+	return 0;
+}
+
+char* strSplit(char* str, char separador)
+{
+	unsigned int i;
+	
+	for(i = 0; str[i] != '\0' && str[i] != separador; i++);
+	
+	if(str[i] == separador)
+		return &str[i+1];
+	else
+		return &str[i];
+}
